d09012e2ec5962a64651e64fe464b0761811139a
ChocoClv <choco_cl@163.com>
2018-03-18 19:08:14 +0800

    图搜索api

--- src/com/choco/Algorithms4edt/Graph/Search.java
+++ src/com/choco/Algorithms4edt/Graph/Search.java
@@ -1,17 +1,11 @@
 package com.choco.Algorithms4edt.Graph;
 
-public abstract class Search {
-    Graph G;
-    int s;
-    Search(Graph G, int s){
-        this.G = G;
-        this.s = s;
-    }
+public interface Search {
 
     //顶点v和s是否连通
-    abstract boolean marked(int v);
+    boolean marked(int v);
 
 
     //与s连通的顶点数
-    abstract int count();
+    int count();
 }
