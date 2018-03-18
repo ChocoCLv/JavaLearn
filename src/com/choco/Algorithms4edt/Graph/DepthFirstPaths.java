408691a84fdbc9aa8d9a94524739eee1712e45f4
ChocoClv <choco_cl@163.com>
2018-03-18 19:09:22 +0800

    深度优先搜索路径

---
+++ src/com/choco/Algorithms4edt/Graph/DepthFirstPaths.java
@@ -0,0 +1,49 @@
+package com.choco.Algorithms4edt.Graph;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+/*
+    从某个节点出发深度优先遍历，构造生成树
+ */
+
+public class DepthFirstPaths extends Paths {
+
+
+    public DepthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        dfs(G, s);
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        int s = Integer.parseInt(args[1]);
+
+        Paths search = new DepthFirstPaths(G, s);
+        for (int v = 0; v < G.V(); v++) {
+            StdOut.print(s + " to " + v + ": ");
+            if (search.hasPathTo(v)) {
+                for (int x : search.pathTo(v)) {
+                    if (x == s)
+                        StdOut.print(x);
+                    else
+                        StdOut.print("-" + x);
+                }
+            }
+            StdOut.println();
+        }
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+}
