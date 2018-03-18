3e8d4f5a1cc752d5fb5f01076c5ac6d0e82b4974
ChocoClv <choco_cl@163.com>
2018-03-18 19:08:28 +0800

    路径API

---
+++ src/com/choco/Algorithms4edt/Graph/Paths.java
@@ -0,0 +1,26 @@
+package com.choco.Algorithms4edt.Graph;
+
+import com.choco.Algorithms4edt.DataStructure.ResizingArrayStack;
+import com.choco.Algorithms4edt.DataStructure.Stack;
+
+public abstract class Paths {
+    boolean[] marked;
+    int[] edgeTo;
+    int s;
+
+    boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v))
+            return null;
+
+        Stack<Integer> path = new ResizingArrayStack<>();
+        for (int x = v; x != s; x = edgeTo[x])
+            path.push(x);
+
+        path.push(s);
+        return path;
+    }
+}
