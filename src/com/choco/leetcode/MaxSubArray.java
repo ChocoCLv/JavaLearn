9044e03e674dfbb3515cfe27610e3886cfd3d640
ChocoCLv <choco_cl@163.com>
2018-04-06 23:52:01 +0800

    爬楼梯

--- src/com/choco/leetcode/MaxSubArray.java
+++ src/com/choco/leetcode/MaxSubArray.java
@@ -4,7 +4,7 @@ public class MaxSubArray {
     public static void main(String[] args) {
         MaxSubArray msa = new MaxSubArray();
         Solution solution = msa.new Solution();
-        System.out.println(solution.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
     class Solution {
@@ -13,14 +13,15 @@ public class MaxSubArray {
 
         public int maxSubArray(int[] nums) {
             M = new int[nums.length];
-            for(int i = 0;i<nums.length;i++){
-                if(i==0){
+            max = nums[0];
+            for (int i = 0; i < nums.length; i++) {
+                if (i == 0) {
                     M[i] = nums[i];
-                }else{
-                    M[i] = Math.max(M[i-1]+nums[i],nums[i]);
+                } else {
+                    M[i] = Math.max(M[i - 1] + nums[i], nums[i]);
                 }
 
-                max = Math.max(M[i],max);
+                max = Math.max(M[i], max);
             }
             return max;
         }
