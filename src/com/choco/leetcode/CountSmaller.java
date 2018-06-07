68ae45ccf7efe4fe13915a900431176fe6bf517f
ChocoCLv <choco_cl@163.com>
2018-06-07 20:12:48 +0800

    计算右侧小于当前元素的个数（效率极低）

--- src/com/choco/leetcode/CountSmaller.java
+++ src/com/choco/leetcode/CountSmaller.java
@@ -1,6 +1,7 @@
 package com.choco.leetcode;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 /***
  *给定一个整数数组 nums，按要求返回一个新数组 counts。
@@ -19,30 +20,31 @@ public class CountSmaller {
     public static void main(String[] args) {
         CountSmaller cs = new CountSmaller();
         Solution solution = cs.new Solution();
-        System.out.println(solution.countSmaller(new int[]{5, 2, 6, 1}));
+        System.out.println(solution.countSmaller(new int[]{26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41}));
     }
 
     class Solution {
-        List<Integer> result;
-        Map<Integer, Integer> resultMap;
-        int[] numsCopy;
+        Element[] elements;
+
         public List<Integer> countSmaller(int[] nums) {
-            result = new ArrayList<>();
-            resultMap = new LinkedHashMap<>();
-            numsCopy = new int[nums.length];
-            int[] aux = new int[nums.length];
-            for(int i = 0;i<nums.length;i++){
-                numsCopy[i] = nums[i];
-                resultMap.put(nums[i],0);
+            elements = new Element[nums.length];
+            Element[] aux = new Element[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                elements[i] = new Element();
+                elements[i].index = i;
+                elements[i].num = nums[i];
+                elements[i].smaller = 0;
             }
-            sortAndCount(nums, aux, 0, nums.length - 1);
-            for(int i:numsCopy){
-                result.add(resultMap.get(i));
+
+            sortAndCount(elements, aux, 0, nums.length - 1);
+            Integer[] result = new Integer[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                result[elements[i].index] = elements[i].smaller;
             }
-            return result;
+            return Arrays.asList(result);
         }
 
-        void sortAndCount(int[] a, int[] aux, int start, int end) {
+        void sortAndCount(Element[] a, Element[] aux, int start, int end) {
             if (start >= end) {
                 return;
             }
@@ -50,20 +52,37 @@ public class CountSmaller {
             int mid = start + (end - start) / 2;
             sortAndCount(a, aux, start, mid);
             sortAndCount(a, aux, mid + 1, end);
-            if (a[mid] <= a[mid + 1])
+            if (a[mid].num <= a[mid + 1].num)
                 return;
             mergeAndCount(a, aux, start, mid, end);
         }
 
-        void mergeAndCount(int[] a, int[] aux, int start, int mid, int end) {
-            for(int k = start;k<=end;k++){
+        void mergeAndCount(Element[] a, Element[] aux, int start, int mid, int end) {
+            for (int k = start; k <= end; k++) {
                 aux[k] = a[k];
             }
             int i = start;
-            int j = mid+1;
-            for(int k = start;k<=end;k++){
-                //对比两个序列的每个元素
+            int j = mid + 1;
+            //归并排序
+            for (int k = start; k <= end; k++) {
+                if (i > mid)
+                    a[k] = aux[j++];
+                else if (j > end)
+                    a[k] = aux[i++];
+                else if (aux[j].num < aux[i].num) {
+                    for(int t = i;t<=mid;t++){
+                        aux[t].smaller++;
+                    }
+                    a[k] = aux[j++];
+                } else
+                    a[k] = aux[i++];
             }
+        }
+
+        class Element {
+            int num;
+            int index;
+            int smaller;
         }
     }
 }
