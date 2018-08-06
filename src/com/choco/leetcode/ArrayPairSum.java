cb9d15b6e447087c853dce92c5a1f07c94e3021a
ChocoCLv <choco_cl@163.com>
2018-08-06 22:05:12 +0800

    561. 数组拆分 I 桶排序 注意可能存在重复元素

--- src/com/choco/leetcode/ArrayPairSum.java
+++ src/com/choco/leetcode/ArrayPairSum.java
@@ -1,27 +1,36 @@
 package com.choco.leetcode;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 /***
  * 数组拆分 I
  * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
  */
 
 public class ArrayPairSum {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ArrayPairSum aps = new ArrayPairSum();
         Solution solution = aps.new Solution();
-        System.out.println(solution.arrayPairSum(new int[]{1,2,3,4}));
+        System.out.println(solution.arrayPairSum(new int[]{1, 2, 3, 4}));
     }
+
     class Solution {
         public int arrayPairSum(int[] nums) {
-            int sum = 0;
-            Arrays.sort(nums);
-            for(int i=0;i<nums.length;i+=2){
-                sum+=nums[i];
+            int[] index = new int[20001];
+            for(int i =0;i<nums.length;i++){
+                index[ nums[i]+10000 ]++;
             }
-            return sum;
+
+            int res = 0;
+            boolean odd = true;
+            for(int i =0;i<index.length;i++){
+                while(index[i]>0){
+                    if(odd){
+                        res +=i-10000;
+                    }
+                    odd = !odd;
+                    index[i]--;
+                }
+            }
+            return res;
         }
     }
 }
