7f5ff270c823288419d6ccaf56b984ea73c97e68
ChocoCLv <choco_cl@163.com>
2018-08-06 16:31:12 +0800

    添加代码文件Q297

---
+++ src/com/choco/leetcode/Codec.java
@@ -0,0 +1,30 @@
+package com.choco.leetcode;
+
+/**
+ * Q297
+ */
+public class Codec {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return "";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+}
