da18858f8b3aac991b12c3ebfff71d05e8de3009
ChocoCLv <choco_cl@163.com>
2018-10-25 21:58:09 +0800

    tmp

---
+++ src/com/choco/leetcode/FourSum.java
@@ -0,0 +1,19 @@
+package com.choco.leetcode;
+
+import java.util.List;
+
+public class FourSum {
+    class Solution {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            return null;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        FourSum fs = new FourSum();
+        Solution solution = fs.new Solution();
+        System.out.println(solution.fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
+    }
+}
