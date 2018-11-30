31e15995a70a3501e12af585ca61b0d61d2a7624
ChocoCLv <choco_cl@163.com>
2018-11-30 12:33:19 +0800

    Q3 无重复字符的最长子串 替换原来的暴力解法 使用动态规划

---
+++ src/test/com/choco/leetcode/LengthOfLongestSubstringTest.java
@@ -0,0 +1,39 @@
+package com.choco.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LengthOfLongestSubstringTest {
+    LengthOfLongestSubstring lols;
+    @Before
+    public void setUp(){
+        lols =new LengthOfLongestSubstring();
+    }
+
+    @Test
+    public void testLengthOfLongestSubstring1() {
+        assertEquals(lols.lengthOfLongestSubstring("abcabcbb"),3);
+    }
+
+    @Test
+    public void testLengthOfLongestSubstring2() {
+        assertEquals(lols.lengthOfLongestSubstring("bbbbb"),1);
+    }
+
+    @Test
+    public void testLengthOfLongestSubstring3() {
+        assertEquals(lols.lengthOfLongestSubstring("pwwkew"),3);
+    }
+
+    @Test
+    public void testLengthOfLongestSubstring4() {
+        assertEquals(lols.lengthOfLongestSubstring(""),0);
+    }
+
+    @Test
+    public void testLengthOfLongestSubstring5() {
+        assertEquals(lols.lengthOfLongestSubstring("dvdf"),3);
+    }
+}
