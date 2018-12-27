ab92e231779f3512c3e66229daad17bf6d1a376b
ChocoCLv <choco_cl@163.com>
2018-12-27 20:22:13 +0800

    Q146 LRU缓存机制

--- src/main/com/choco/leetcode/LRUCache.java
+++ src/main/com/choco/leetcode/LRUCache.java
@@ -1,23 +1,21 @@
 package com.choco.leetcode;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
 
 /**
- * Q  LRU缓存机制
+ * Q146 LRU缓存机制
  * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
- *
+ * <p>
  * 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
  * 写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，它应该在写入新数据之前删除最近最少使用的数据值，从而为新的数据值留出空间。
- *
+ * <p>
  * 进阶:
- *
+ * <p>
  * 你是否可以在 O(1) 时间复杂度内完成这两种操作？
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * LRUCache cache = new LRUCache( 2 缓存容量  );
  * cache.put(1, 1);
  * cache.put(2, 2);
@@ -30,53 +28,86 @@ import java.util.PriorityQueue;
  * cache.get(4);       // 返回  4
  */
 public class LRUCache {
-    private Map<Integer,Element> cache;
-    private int lruKey;
-
-    private PriorityQueue lruQueue;
+    LRUNode head;
+    LRUNode tail;
     private int capacity;
-    class Element{
-        int key;
-        int value;
-        int times;
-
-        public Element(int key, int value, int times) {
-            this.key = key;
-            this.value = value;
-            this.times = times;
-        }
-    }
-
+    private int size;
+    private Map<Integer, LRUNode> map;
     public LRUCache(int capacity) {
-        cache = new HashMap<>();
-        this.capacity=capacity;
-        lruQueue = new PriorityQueue(new Comparator<Element>() {
-            @Override
-            public int compare(Element o1, Element o2) {
-                return o2.times-o1.times;
-            }
-        });
+        this.capacity = capacity;
+        this.size = 0;
+        map = new HashMap<>(capacity);
     }
 
     public int get(int key) {
-        if(cache.containsKey(key))
-            return cache.get(key).value;
-        return -1;
+        LRUNode node = map.getOrDefault(key,null);
+        if(node == null)
+            return -1;
+        head = moveNodeToHead(node);
+        return node.value;
     }
 
-    //堆排序
+    private LRUNode moveNodeToHead(LRUNode node) {
+        if(node == head)
+            return node;
+        //将被访问的节点移动到head
+        node.pre.next = node.next;
+        if (node.next != null) {
+            node.next.pre = node.pre;
+        } else {
+            //说明当前节点为尾节点
+            tail = tail.pre;
+        }
+        node.next = head;
+        head.pre = node;
+        head = node;
+        return head;
+    }
+
     public void put(int key, int value) {
-        if(cache.containsKey(key)){
-            Element ele = cache.get(key);
-            ele.times++;
-            ele.value=value;
-            ele.key = key;
-        }else{
-            if(cache.size()==capacity){
-                Element ele = (Element)lruQueue.poll();
-                cache.remove(ele.key);
-            }
-            cache.put(key,new Element(key,value,0));
+        //为空
+        if (head == null) {
+            head = new LRUNode(key, value);
+            tail = head;
+            map.put(key,head);
+            size++;
+            return;
+        }
+        //key已存在
+        if(map.containsKey(key)){
+            //将被访问的节点移动到head
+            LRUNode node = map.get(key);
+            node.value = value;
+            head = moveNodeToHead(node);
+            return;
+        }
+
+        //插入新key
+        //在头部插入节点
+        head.pre = new LRUNode(key, value);
+        head.pre.next = head;
+        head = head.pre;
+        size++;
+        map.put(key,head);
+        if (size > capacity) {
+            //删除尾节点
+            map.remove(tail.key);
+            tail = tail.pre;
+            tail.next = null;
+            size--;
+        }
+
+    }
+
+    class LRUNode {
+        LRUNode pre;
+        LRUNode next;
+        int key;
+        int value;
+
+        LRUNode(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
 }
--- src/test/com/choco/leetcode/LRUCacheTest.java
+++ src/test/com/choco/leetcode/LRUCacheTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class LRUCacheTest {
 
     @Test
-    public void testPut() {
+    public void testPut1() {
         LRUCache cache = new LRUCache(2);
         cache.put(1, 1);
         cache.put(2, 2);
@@ -18,6 +18,26 @@ public class LRUCacheTest {
         assertEquals(cache.get(1), -1);       // 返回 -1 (未找到)
         assertEquals(cache.get(3), 3);     // 返回  3
         assertEquals(cache.get(4), 4);      // 返回  4
+        cache.put(3,5);
+        assertEquals(cache.get(3), 5);      // 返回  4
     }
 
+    @Test
+    public void testPut2(){
+        LRUCache cache = new LRUCache(1);
+        cache.put(2, 1);
+        assertEquals(cache.get(2), 1);// 返回  1
+    }
+
+    @Test
+    public void testPut3(){
+        LRUCache cache = new LRUCache(2);
+        assertEquals(cache.get(2), -1);    // 返回 -1 (未找到)
+        cache.put(2,6);
+        assertEquals(cache.get(1), -1);       // 返回 -1 (未找到)
+        cache.put(1,5);
+        cache.put(1,2);
+        assertEquals(cache.get(1), 2);       // 返回 -1 (未找到)
+        assertEquals(cache.get(2), 6);    // 返回 -1 (未找到)
+    }
 }
