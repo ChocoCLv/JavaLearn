846eb75bc0f8763f2ca5f994667fa8adc8351e73
ChocoCLv <choco_cl@163.com>
2019-01-08 22:24:56 +0800

    Q7 整数反转

--- src/main/com/choco/leetcode/ReverseInteger.java
+++ src/main/com/choco/leetcode/ReverseInteger.java
@@ -1,7 +1,24 @@
 package com.choco.leetcode;
 
 /**
- * Created by choco on 2018/3/26.
+ * Q7 整数反转
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ *
+ * 示例 1:
+ *
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ *
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ *
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ *
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
  */
 public class ReverseInteger {
     public static void main(String[] args) {
@@ -13,33 +30,15 @@ public class ReverseInteger {
 
     class Solution {
         public int reverse(int x) {
-            int reverse = 0;
-            int[] digit = new int[10];
-            int size;
-            int scale = 1;
-            int[] maxs = {2, 1, 4, 7, 4, 8, 3, 6, 4, 7};
-
-            int before = Math.abs(x);
-            if (before < 0)
-                return 0;
-            for (size = 0; before != 0; size++) {
-                digit[size] = before % 10;
-
-                if (Math.abs(x) >= 1000000000) {
-                    if (digit[size] > maxs[size])
-                        return 0;
-                }
-
-                before /= 10;
+            int rev = 0;
+            while (x != 0) {
+                int pop = x % 10;
+                x /= 10;
+                if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+                if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+                rev = rev * 10 + pop;
             }
-            size--;
-            while (size >= 0) {
-                reverse += digit[size] * scale;
-                scale *= 10;
-                size--;
-            }
-
-            return x < 0 ? (-reverse) : reverse;
+            return rev;
         }
     }
 }
